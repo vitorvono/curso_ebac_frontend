c5a73cb785099091256ede9247b4d3f615cb839f
Vitor Vono <vitorvono@gmail.com>
2023-05-04 09:08:11 -0300

    Update main.js
    Update - 4 de maio de 2023.

--- main.js
+++ main.js
@@ -1,5 +1,44 @@
 $(document).ready(function () {
-  $("#telefone").mask("(00)00000-0000");
-  $("#cpf").mask("000.000.000-00");
-  $("#cep").mask("00000-000");
+  $("form").validate({
+    rules: {
+      nomeCompleto: {
+        required: true,
+      },
+      email: {
+        required: true,
+      },
+      telefone: {
+        required: true,
+      },
+      cpf: {
+        required: true,
+      },
+      endereco: {
+        required: true,
+      },
+      cep: {
+        required: true,
+      },
+    },
+    messages: {
+      nomeCompleto: "Por favor, informe seu nome completo",
+      email: "Por favor, informe um email válido",
+      telefone: "Por favor, numero de telefone obrigatório",
+      cpf: "Por favor, CPF obrigatório",
+      endereco: "Por favor, endereço é obrigatório",
+      cep: "Por favor, CEP é obrigatório",
+    },
+  });
+
+  $("#telefone").mask("(00) 00000-0000", {
+    placeholder: "(00)00000-0000",
+  });
+
+  $("#cpf").mask("000.000.000-00", {
+    placeholder: "000.000.000-00",
+  });
+
+  $("#cep").mask("00000-000", {
+    placeholder: "00000-000",
+  });
 });
