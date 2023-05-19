a75dae10034800e0d2bfc5d6a1178fc9ff748c45
Vitor Vono <vitorvono@gmail.com>
2023-05-19 09:58:47 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function Token(hobbit, anao, elfo) {
+    this.hobbit = hobbit
+    this.anao = anao
+    this.elfo = elfo
+}
+
+const universo = new Token('Bilbo', ['Torin', 'Gimli', 'Balin'], ['Legolas', 'Arwen', 'Elrond'])
+console.log(token)
